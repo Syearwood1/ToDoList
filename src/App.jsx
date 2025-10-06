4502ec01b2c8a206d20b8be697efeb3f5eef71c5
Syearwood1 <90792292+Syearwood1@users.noreply.github.com>
2025-10-06 14:22:35 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+  const [text, settext] = useState('')
+  const [list, setList] = useState([])
+  const task = { //creates object called task
+    text: text,
+    completed: false,
+  };
+  
+  const textAdd = () => {
+    if(text.trim() !== ''){ //If text isnt blank continue
+      setList([...list, task]); //add text(String) to list(array) with all current texts
+      settext('') //Make text blank again
+    }
+  }
+  const clearList = () =>{
+    setList([])
+  }
+
+  return (
+    <>
+      <h1>ToDoList</h1>
+      <div>
+      <input 
+        type='text' 
+        placeholder='Enter a text'
+        value={text}
+        onChange={(e)=>settext(e.target.value)}
+        />
+      <button onClick={textAdd}>+</button>
+      <p>Our Current text: {text} </p>
+      <ul>
+        {list.map((item, index)=>(  //map iterated through all elements of an array
+          <li
+            key={index}
+            >
+              {item.text}
+              </li> // List Item.text (string)
+        ))}
+      </ul>
+      <button onClick={clearList}>
+      Clear List
+      </button>
+      </div>
+      <p>When hovering an X should appear on the right allowing deletion</p>
+      
+    </>
+  )
+}
+
+export default App
